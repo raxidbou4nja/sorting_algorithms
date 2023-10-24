5110df40feed2c17bc859ec1dfa09fbd5bb9dd6d
raxidbou4nja <rachid.boughnja@gmail.com>
2023-10-24 07:39:24 -0700

    add prototypes

--- sort.h
+++ sort.h
@@ -27,8 +27,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 
 void quick_sort(int *array, size_t size);
-
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
