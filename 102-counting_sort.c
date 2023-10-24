fa5af492cfd9112468239d23b31f2eb6e28de4c3
raxidbou4nja <rachid.boughnja@gmail.com>
2023-10-24 08:18:46 -0700

    fix errors

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,49 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * counting_sort - sorts an array of integers in ascending order using
- * the Counting sort algorithm.
- * @array: array to be sorted.
- * @size: size of an array.
+ * the Counting Sort algorithm.
+ * @array: Array to be sorted.
+ * @size: Size of the array.
  */
 void counting_sort(int *array, size_t size)
 {
-	int max = array[0];
-	int i, *counting_array, j = 0;
+	int *output = NULL, max_value = 0, *count_arr = NULL;
+	size_t i;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || size <= 1)
 		return;
 
-	for (i = 1; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		if (array[i] > max)
-			max = array[i];
+		if (array[i] > max_value)
+			max_value = array[i];
 	}
-
-	counting_array = (int *)malloc(sizeof(int) * (max + 1));
-
-	if (counting_array == NULL)
+	count_arr = malloc((max_value + 1) * sizeof(int));
+	if (count_arr == NULL)
 		return;
 
-	for (i = 0; i <= max; i++)
-		counting_array[i] = 0;
+	for (i = 0; i <= (size_t)max_value; i++)
+		count_arr[i] = 0;
+	for (i = 0; i < size; i++)
+		count_arr[array[i]]++;
+	for (i = 1; i <= (size_t)max_value; i++)
+		count_arr[i] += count_arr[i - 1];
 
-	for (i = 0; i < (int)size; i++)
-		counting_array[array[i]]++;
-
-	print_array(counting_array, max + 1);
-
-	for (i = 0; i <= max; i++)
+	output = malloc(size * sizeof(int));
+	if (output == NULL)
 	{
-		while (counting_array[i] > 0)
-		{
-			array[j] = i;
-			j++;
-			counting_array[i]--;
-		}
+		free(count_arr);
+		return;
 	}
+	for (i = 0; i < size; i++)
+	{
+		output[count_arr[array[i]] - 1] = array[i];
+		count_arr[array[i]]--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
 
-	free(counting_array);
+	print_array(count_arr, max_value + 1);
+	free(count_arr);
+	free(output);
 }
